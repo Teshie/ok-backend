9c15d75dd745778ca9b80611585b42bff21c36dc
Teshie <teshomeaychiluhem@gmail.com>
2022-12-06 23:10:30 -0800

    remove orders

--- billings/admin.py
+++ billings/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product, Store, Cart, Order
+from .models import Category, Product, Store, Cart
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Store)
 admin.site.register(Cart)
-admin.site.register(Order)
